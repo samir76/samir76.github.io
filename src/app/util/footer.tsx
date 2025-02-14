482d0c6822fd70e11f3880200ce1f5289c8929a0
samir76 <abbasumar855@yahoo.com>
2025-02-14 23:42:14 +1100

    next v1.02

--- src/app/util/footer.tsx
+++ src/app/util/footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
     <div className="border-grid border-t mt-3 md:px-8 sticky bottom-0 left-0 max-w-full w-full h-[100] bg-white">
       <div className="container py-4">
         <div className="text-center text-sm text-muted-foreground mt-2">
-          &copy; 2021 Samir Ibrahim. 
+          &copy; 2025 Samir Ibrahim. 
         </div>
         <div className="flex justify-center gap-4 mt-2">
           <Link href="https://github.com/samir76/">
