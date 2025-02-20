727367572b498656bb872e844114384812fc72ef
samir76 <abbasumar855@yahoo.com>
2025-02-20 20:24:44 +1100

    v2.5.5 - home edit II

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,9 +32,9 @@ export default function Home() {
 
         <hr className="mb-4"/>
 
-      {/* <ImageGrid images={cardImages.slice(1,4)} /> */}
-
       <ImageGrid images={[cardImages[0], cardImages[1], cardImages[3]]} />
+
+      {/* <ImageGrid images={cardImages.slice(1,4)} /> */}
 
       </div>
     </main>
