7436942b36f69ce0a0fcff5314baa87d3b64e63e
samir76 <abbasumar855@yahoo.com>
2025-02-20 11:44:10 +1100

    V2.5.2 - edit more testing data

--- src/app/components/CardImagesComponent.tsx
+++ src/app/components/CardImagesComponent.tsx
@@ -6,7 +6,7 @@ const CardImagesComponent = () => {
     <div data-testid="card-images">
       {cardImages.map((image, index) => (
         <div key={index}>
-          <Image src={image.src} alt={image.alt} width={300} height={200} />
+          <Image src={image.src} alt={image.alt} width={300} height={200}/>
           <p>{image.alt}</p>
         </div>
       ))}
